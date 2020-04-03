870f6f053aa775fffc72cec3e8a6e5b39efa6d2f
jaymar85 <jermark851@gmail.com>
2020-04-03 13:59:52 -0500

    Initial Commit

---
+++ AFSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AFSample
+//
+//  Created by Jeremy Mark on 4/3/20.
+//  Copyright © 2020 Jeremy Mark. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
